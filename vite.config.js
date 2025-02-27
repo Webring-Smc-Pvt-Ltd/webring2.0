bc2bedfd84df8ea351c33fc7b33e9c898e8a2ddc
Abubakar <Abubakar@ANAS9T7>
2025-02-27 14:19:23 +0500

    change in viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  build:{
-    chunkSizeWarningLimit:1000
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Moves all node_modules code to a separate file
+          }
+        }
+      }
+    }
   }
-})
+});
