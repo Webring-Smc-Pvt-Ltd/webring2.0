86f6ca2edf45814b62d722434af2a76954c1eddb
Abubakar <Abubakar@ANAS9T7>
2025-02-27 15:24:54 +0500

    rollback files

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Moves all node_modules code to a separate file
-          }
-        }
-      }
-    }
-  }
-});
+  plugins: [react()],
+})
